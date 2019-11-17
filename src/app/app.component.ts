2eff22111d3e7ef91cf15cdf25b64f305d2438fb
Demira <d.dimitrova72@gmail.com>
2019-11-17 19:03:03 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 
+
+import currencies from 'src/assets/json/currencies.json';
+import countries from 'src/assets/json/countries.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lol';
+
+  title = 'november1-front-end';
+
+
+
+  constructor() { }
+  curr: any = currencies;
+  countrs: any = countries;
+
+
+  selectedCurr = '';
+  selectedCoun = '';
+
+  onEnterCoun(evt: any, s: string) {
+    if (evt.source.selected) {
+      this.selectedCoun = s;
+    }
+  }
+
+  onEnterCurr(evt: any, s: string) {
+    if (evt.source.selected) {
+      this.selectedCurr = s;
+    }
+  }
+
+
+  selectCurrency(value: string) {
+    this.selectedCurr = value;
+  }
+
+  selectCountry(value: string) {
+    this.selectedCoun = value;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {
+  MatButtonModule, MatCardModule, MatCheckboxModule,
+  MatChipsModule, MatDialogModule, MatIconModule,
+  MatInputModule,
+  MatListModule, MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSidenavModule, MatSnackBarModule,
+  MatTableModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTableModule,
+    MatIconModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
