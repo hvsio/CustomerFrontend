186a1384cad75675845e5dd6aebff65528807503
Demira <d.dimitrova72@gmail.com>
2019-12-06 23:20:57 +0100

    hope it works in cloud now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import {MatTableDataSource} from '@angular/material';
 import {MatIconRegistry} from '@angular/material/icon';
 import {DomSanitizer} from '@angular/platform-browser';
 import {N1Service} from './n1.service';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -25,6 +26,16 @@ export class AppComponent implements OnInit {
     this.registry.addSvgIcon(`question-mark`, this.domSanitizer.bypassSecurityTrustResourceUrl('./assets/img/question-mark.svg'));
     this.registry.addSvgIcon(`express-transfer`, this.domSanitizer.bypassSecurityTrustResourceUrl('./assets/img/express-transfer.svg'));
   }
+
+  amount = new FormControl('',
+    [Validators.required,
+      Validators.min(1000),
+      Validators.max(10000000)]);
+
+  frequency = new FormControl('',
+    [Validators.required,
+      Validators.min(1),
+      Validators.max(10000000)]);
 
   toCountries: any = countries;
 
@@ -45,8 +56,7 @@ export class AppComponent implements OnInit {
 
   ifServiceAvailable: boolean;
   submitSent = false;
-  isLoading: boolean;
-  isLoading = false;
+  isLoading: boolean = false;
   isLoaded = false;
   bankSuppliers: any;
 
@@ -55,29 +65,33 @@ export class AppComponent implements OnInit {
   private arr: { [p: string]: unknown }[];
 
   submitForm(amountMoney: string, amountTransactions: string) {
-    this.isLoading = true;
-    this.submitSent = true;
-    this.service.getCalculations(this.selectedCountryFrom, this.selectedCountryTo,
-      this.selectedCurrencyFrom, this.selectedCurrencyTo,
-      amountMoney, amountTransactions).subscribe((response) => {
-        this.bankSuppliers = response['body'];
-        this.bankSuppliers.sort((a, b) => a['totalCost'] === b['totalCost'] ? 0 : a['totalCost'] > b['totalCost'] ? 1 : -1);
-        console.log(this.bankSuppliers);
-        this.results = new MatTableDataSource(this.bankSuppliers);
-        this.isLoading = false;
-        this.isLoaded = true;
+    if (this.getAmountTruth() && this.getFrequencyTruth() && this.selectedCountryFrom !== '' && this.selectedCountryTo !== '' &&
+      this.selectedCurrencyFrom !== '' && this.selectedCurrencyTo !== '') {
+      this.isLoading = true;
+      this.submitSent = true;
+      this.service.getCalculations(this.selectedCountryFrom, this.selectedCountryTo,
+        this.selectedCurrencyFrom, this.selectedCurrencyTo,
+        amountMoney, amountTransactions).subscribe((response) => {
+          this.bankSuppliers = response['body'];
+          this.bankSuppliers.sort((a, b) => a['totalCost'] === b['totalCost'] ? 0 : a['totalCost'] > b['totalCost'] ? 1 : -1);
+          console.log(this.bankSuppliers);
+          this.results = new MatTableDataSource(this.bankSuppliers);
+          this.isLoading = false;
+          this.isLoaded = true;
 
-        Object.keys(this.bankSuppliers).some(key => {
-            console.log(this.bankSuppliers[key]['name']);
-            if (this.bankSuppliers[key]['name'] === 'November First') {
-              this.NOVEMBER_FIRST_COST = this.bankSuppliers[key]['totalCost'];
-              console.log(this.NOVEMBER_FIRST_COST);
+          Object.keys(this.bankSuppliers).some(key => {
+              console.log(this.bankSuppliers[key]['name']);
+              if (this.bankSuppliers[key]['name'] === 'November First') {
+                this.NOVEMBER_FIRST_COST = this.bankSuppliers[key]['totalCost'];
+                console.log(this.NOVEMBER_FIRST_COST);
+              }
             }
-          }
-        );
-      }
-    );
+          );
+        }
+      );
+    }
   }
+
 
   getN1Countries() {
     this.N1service.getAvailableCountries().subscribe(
@@ -87,7 +101,7 @@ export class AppComponent implements OnInit {
         } else {
           this.ifServiceAvailable = true;
         }
-        this.fromCountries = Object.entries(data).map(([k, v]) => ( { country: v, abbreviation: k } ));
+        this.fromCountries = Object.entries(data).map(([k, v]) => ({country: v, abbreviation: k}));
         console.log(this.fromCountries);
       }
     );
@@ -97,14 +111,14 @@ export class AppComponent implements OnInit {
     console.log(chosenCountry);
     this.N1service.getAvailableCurrencies(chosenCountry).subscribe(
       data => {
-        this.fromCurrency = Object.entries(data['FromCurrencies']).map(([k, v]) => ( { currency: v } ));
-        this.toCurrency = Object.entries(data['ToCurrencies']).map(([k, v]) => ( { currency: v } ));
+        this.fromCurrency = Object.entries(data['FromCurrencies']).map(([k, v]) => ({currency: v}));
+        this.toCurrency = Object.entries(data['ToCurrencies']).map(([k, v]) => ({currency: v}));
         console.log(data['ToCurrencies']);
         console.log(this.fromCurrency);
       });
   }
 
-  checkName( fromCurrency: string ) {
+  checkName(fromCurrency: string) {
     return fromCurrency;
   }
 
@@ -166,6 +180,31 @@ export class AppComponent implements OnInit {
     this.selectedCountryTo = value;
     console.log(this.selectedCountryTo);
   }
+
+  getErrorMessageAmount(): string {
+    return this.amount.hasError('required') ? 'You must enter a value' :
+      this.amount.hasError('min') ? 'Amount of money must be more than 1000' :
+        this.amount.hasError('max') ? 'Amount of money must be less than 10 000 000' : '';
+  }
+
+  getAmountTruth(): boolean {
+    return this.amount.hasError('required') ? false :
+      this.amount.hasError('min') ? false :
+        this.amount.hasError('max') ? false : true;
+  }
+
+  getErrorMessageFrequency(): string {
+    return this.frequency.hasError('required') ? 'You must enter a value' :
+      this.frequency.hasError('min') ? 'Frequency must be more than 1' :
+        this.frequency.hasError('max') ? 'Frequency must be less than 10 000 000' : '';
+  }
+
+  getFrequencyTruth(): boolean {
+    return this.frequency.hasError('required') ? false :
+      this.frequency.hasError('min') ? false :
+        this.frequency.hasError('max') ? false : true;
+  }
+
 
   ngOnInit(): void {
     this.getN1Countries();
