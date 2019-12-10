8095e9afaf0a97299a4463929179344704f7e479
Demira <d.dimitrova72@gmail.com>
2019-12-10 15:09:59 +0100

    info when error ocurred, no eternal loading screen

--- src/app/calculator.service.ts
+++ src/app/calculator.service.ts
@@ -1,10 +1,11 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import {HttpClient, HttpErrorResponse, HttpEvent, HttpHeaders, HttpResponse} from '@angular/common/http';
+import {Observable, of, throwError} from 'rxjs';
+
 import {catchError, retry} from 'rxjs/operators';
 import {environment} from 'src/environments/environment';
 
-const SERVER_URL = environment.comparatorBackend ;
+const SERVER_URL = environment.comparatorBackend;
 
 @Injectable({
   providedIn: 'root',
@@ -20,8 +21,8 @@ export class CalculatorService {
                   fromCurrency: string,
                   toCurrency: string,
                   amountMone: string,
-                  amountTransaction: string): Observable<HttpResponse<Object>> {
-    return this.http.get( `${SERVER_URL}` + '/banksuppliers', {
+                  amountTransaction: string): Observable<HttpResponse<any>> {
+    return this.http.get(`${SERVER_URL}` + '/banksuppliers', {
       params: {
         fromCountry: fromCountr,
         toCountry: toCountr,
@@ -30,17 +31,34 @@ export class CalculatorService {
         volume: amountMone,
         nrTransactions: amountTransaction
       }, observe: 'response'
-    });
+    }).pipe(catchError((err: HttpErrorResponse) => {
+
+      if (err.error instanceof Error) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', err.error.message);
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        console.error(`Backend returned code ${err.status}, body was: ${err.error}`);
+      }
+
+      // ...optionally return a default fallback value so app can continue (pick one)
+      // which could be a default value
+      // return Observable.of<any>({my: "default value..."});
+      // or simply an empty observable
+      return throwError('Something bad happened; please try again later.');
+    }));
   }
 
 
-  handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(`${error.error.errors}`);
-      this.setResponse(`${error.error.errors}`);
-      return of(result as T);
-    };
-  }
+  //
+  // handleError<T>(operation = 'operation', result?: T) {
+  //   return (error: any): Observable<T> => {
+  //     console.log(`${error.error}`);
+  //     this.setResponse(`${error.error}`);
+  //     return of(result as T);
+  //   };
+  // }
 
   setResponse(text: string) {
     this.errorResponse = text;
@@ -49,4 +67,8 @@ export class CalculatorService {
   getResponse() {
     return this.errorResponse;
   }
+
 }
+
+
+
